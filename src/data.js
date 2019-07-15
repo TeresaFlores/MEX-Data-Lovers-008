9a7fa8f33d0aa9d0663e34b0d1b134dbb39f03c6
TeresaFlores <teresaflores1730@gmail.com>
2019-07-14 22:26:39 -0500

    Se creo index2.html, grid.css y se agregaron cambios en index.html y style.css. En index.html y index2.html se agrego informacion sobre pagina. En style.css se realizaron cambios para grid. En grid.css se realizaron todos los diseños para una grid y se agrego @media para responsive

--- src/data.js
+++ src/data.js
@@ -1,27 +1,80 @@
-// esta es una función de ejemplo
-// puedes ver como agregamos la función a nuestro objeto global window
-
-const mostrar= () => {
-  let pokemon= POKEMON.pokemon;
+window.data ={  
   
-  for(i=0; i<= pokemon.length;i++) {
-    let numero= pokemon[i].num;
-    let nombre= pokemon[i].name;
-    let imagen= pokemon[i].img;
-    let tipo= pokemon[i].type;
-    let debilidades= pokemon[i].weaknesses;
-    //document.write(numero);
-    //document.write(nombre);
-    //document.write(imagen);
-    //document.write(tipo);
-    //document.write(debilidades);
+  show: (pokemon) => {
+  let cardPokemon= []; //variable vacia para crear array con nueva informacion   
   
+  for(let i=0; i< pokemon.length;i++) { //bucle para buscar en la data informacion especifica
+    let number= pokemon[i].num;
+    let name= pokemon[i].name;
+    let image= pokemon[i].img;
+    let type= pokemon[i].type;
+    let debility= pokemon[i].weaknesses;     
+    
+    //hacer nuevo array y añadir cada elemento
+    cardPokemon.push(
+      {
+        number,
+        name,
+        image,
+        type,
+        debility
+      }
+    ); 
+    
   }
-  
-  //return 'pokemon';
+  return cardPokemon; 
 
+}
 };
-mostrar();
 
-// mostrar();
-window.mostrar = mostrar;
+
+
+// // const showPokemon = window.data.show(POKEMON.pokemon);
+
+      
+
+// //       const total=  (totalHierba, totalVeneno, totalFuego, totalVuelo, totalAgua) => {
+// //         total <= 20; 
+// //         console.log(total);
+// //       }
+
+// computeStats: (type)=> {
+//     const hierba = showPokemon.filter(showPokemon => showPokemon.type.includes("Grass"));
+//       const totalHierba= hierba.lengt;
+//     const veneno = showPokemon.filter(showPokemon => showPokemon.type.includes("Posison"));
+//       const totalVeneno= veneno.length;
+//     const fuego = showPokemon.filter(showPokemon => showPokemon.type.includes("Fire"));
+//       const totalFuego= fuego.length;
+//     const volador = showPokemon.filter(showPokemon => showPokemon.type.includes("Flying"));
+//      const totalVuelo= volador.length;
+//     const agua = showPokemon.filter(showPokemon => showPokemon.type.includes("Water"));
+//      const totalAgua= agua.length;
+//     const electrico = showPokemon.filter(showPokemon => showPokemon.type.includes("Electric"));
+//       const totalElectrico= electrico.length;
+//     const insecto = showPokemon.filter(showPokemon => showPokemon.type.includes("Bug"));
+//       const totalInsecto= insecto.length;
+//     const roca = showPokemon.filter(showPokemon => showPokemon.type.includes("Rock"));
+//      const totalRoca= roca.length;
+//     const hielo = showPokemon.filter(showPokemon => showPokemon.type.includes("Ice"));
+//       const totalHielo= hielo.length;
+//     const psyquico = showPokemon.filter(showPokemon => showPokemon.type.includes("Psychic"));
+//       const totalPsyquico= psyquico.length;
+//     const normal = showPokemon.filter(showPokemon => showPokemon.type.includes("Normal"));
+//       const totalNormal= normal.length;
+//     const tierra= showPokemon.filter(showPokemon => showPokemon.type.includes("Ground"));
+//       const totalTierra= tierra.length;
+//     const dragon = showPokemon.filter(showPokemon => showPokemon.type.includes("Dragon"));
+//       const totalDragon= dragon.length;
+//     const fantasma = showPokemon.filter(showPokemon => showPokemon.type.includes("Ghost"));
+//       const totalFantasma= fantasma.length;
+//     const oscuro = showPokemon.filter(showPokemon => showPokemon.type.includes("Dark"));
+//       const totalOscuro= oscuro.length;
+//     const lucha = showPokemon.filter(showPokemon => showPokemon.type.includes("Fighting"));
+//       const totalLucha= lucha.length;
+
+
+// const totales= () => {
+
+// return totalHielo,totalFuego, totalVuelo  <= 20;
+// }
+// console.log (totales); 
--- src/main.js
+++ src/main.js
@@ -1,30 +1,80 @@
-let mostarPokemon= POKEMON.pokemon;
-let information= document.getElementById("col-10 contenido-cards");
-//Mostrando tarjetas de pokemon
-mostarPokemon.forEach(function(element)  {
-  //Diseño de tarjetas y contenido
-  let cards = `<div class="tarjetas">
-  <img src="${element.img}">
-  <p id="num" >No. ${element.num}</p>
-  <p id= "nombre">${element.name}</p>
-  <p id= "tipo">${element.type}</p>
-  <p id="debilidad">Debilidad: ${element.weaknesses}</p>
-</div>`
-//Llamando variable para mostrar
-  information.insertAdjacentHTML("beforeEnd", cards);
-});
+let information= document.getElementById("contenido-cards");
+const showPokemon = window.data.show(POKEMON.pokemon);//llamando data de archivo data.js
+const select = document.getElementById("select-type-pokemon");//llamando  a select
+let printCards = (showPokemon) => {
+ let string = "";
+ showPokemon.forEach(element => { 
+
+   string += `<div class="tarjetas">
+   <img src="${element.image}">
+   <p id="num" >No. ${element.number}</p>
+   <p id= "nombre">${element.name}</p>
+   <p id= "tipo">${element.type}</p>
+   <p id="debilidad">Debilidad: ${element.debility}</p>
+ </div>`
+ })
+ information.innerHTML = string;
+//console.log(printCards);
+};
+printCards(showPokemon);
+
+
 
 //Funcion para filtrar por tipo
-const pasto= mostarPokemon.filter(mostarPokemon => mostarPokemon.type.includes("Grass"));
-//console.log(pasto)
-const insecto= mostarPokemon.filter(mostarPokemon => mostarPokemon.type.includes("Bug"));
-//console.log(insecto)
-const hielo= mostarPokemon.filter(mostarPokemon => mostarPokemon.type.includes("Ice"));
-//console.log(hielo)
-const suelo= mostarPokemon.filter(mostarPokemon => mostarPokemon.type.includes("Ground"));
-//console.log(suelo)
-const lucha= mostarPokemon.filter(mostarPokemon => mostarPokemon.type.includes("Fighting"));
-//console.log(lucha)
-const hada= mostarPokemon.filter(mostarPokemon => mostarPokemon.type.includes("Fairy"));
-console.log(hada)
+let arrayType = (showPokemon) => {
+  const newArray = showPokemon.filter(pokemon => pokemon.type[0] == select.value);  
+
+  if(newArray == "Todos") {
+    //printCards(showPokemon);
+    printCards(showPokemon);
+  }else{
+    //console.log(newArray);
+    printCards(newArray);  
+  }
+ }
+
+ select.addEventListener("change", () => {
+  arrayType(showPokemon)
+ });
+
+
+ 
+
+
+ 
+// //agregar una tabla a html
+//     function genera_tabla() {
+//       // Obtener la referencia del elemento body
+//       var body = document.getElementsByTagName("body")[0];
+     
+//       // Crea un elemento <table> y un elemento <tbody>
+//       var tabla   = document.createElement("table");
+//       var tblBody = document.createElement("tbody");
+     
+//       // Crea las celdas
+//       for (var i = 0; i < 17; i++) {
+//         // Crea las hileras de la tabla
+//         var hilera = document.createElement("tr");
+     
+//         for (var j = 0; j < 2; j++) {
+//           // Crea un elemento <td> y un nodo de texto, haz que el nodo de
+//           // texto sea el contenido de <td>, ubica el elemento <td> al final
+//           // de la hilera de la tabla
+//           var celda = document.createElement("td");
+//           var textoCelda = document.createTextNode("celda en la hilera "+i+", columna "+j);
+//           celda.appendChild(textoCelda);
+//           hilera.appendChild(celda);
+//         }
+     
+//         // agrega la hilera al final de la tabla (al final del elemento tblbody)
+//         tblBody.appendChild(hilera);
+//       }
+     
+//       // posiciona el <tbody> debajo del elemento <table>
+//       tabla.appendChild(tblBody);
+//       // appends <table> into <body>
+//       body.appendChild(tabla);
+//       // modifica el atributo "border" de la tabla y lo fija a "2";
+//       tabla.setAttribute("border", "1");
+//     }
 
